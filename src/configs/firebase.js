3857c01401aafd43bbc99d1f88849a3b054b0d99
VanMerr <vanmerr020403@gmail.com>
2025-05-24 21:38:02 +0700

    update code

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,12 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, FacebookAuthProvider, GoogleAuthProvider } from 'firebase/auth';
-import serviceKey from './serviceKey.json';
 
 const firebaseConfig = {
-  apiKey: serviceKey.apiKey,
-  authDomain: serviceKey.authDomain,
-  projectId: serviceKey.projectId,
-  appId: serviceKey.appId,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
